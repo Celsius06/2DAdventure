3dc2677b2ca44d0825713233aac0ea023dde7366
Celsius06 <minhthuan24062004@gmail.com>
2023-12-16 01:11:03 +0700

    Modification

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 2D-Adventure
-My repository to store my group's 2D-based game project: The Adventure of Frogi the Frost King
+# 2DGame_test
+Test space for my 2D-based game
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -43,14 +43,14 @@ public class Player extends Entity {
     }
     public void getPlayerImage() {
         try {
-            up1= ImageIO.read(new File("res/player/FroGi_up1.png"));
-            up2=ImageIO.read(new File("res/player/FroGi_up2.png"));
-            down1=ImageIO.read(new File("res/player/FroGi_down1.png"));
-            down2=ImageIO.read(new File("res/player/FroGi_down2.png"));
-            left1=ImageIO.read(new File("res/player/FroGi_left3.png"));
-            left2=ImageIO.read(new File("res/player/FroGi_left2.png"));
-            right1=ImageIO.read(new File("res/player/FroGi_right3.png"));
-            right2=ImageIO.read(new File("res/player/FroGi_right2.png"));
+            up1 = ImageIO.read(new File("res/player/FroGi_up1.png"));
+            up2 = ImageIO.read(new File("res/player/FroGi_up2.png"));
+            down1 = ImageIO.read(new File("res/player/FroGi_down1.png"));
+            down2 = ImageIO.read(new File("res/player/FroGi_down2.png"));
+            left1 = ImageIO.read(new File("res/player/FroGi_left3.png"));
+            left2 = ImageIO.read(new File("res/player/FroGi_left2.png"));
+            right1 = ImageIO.read(new File("res/player/FroGi_right3.png"));
+            right2 = ImageIO.read(new File("res/player/FroGi_right2.png"));
 
         } catch(IOException e) {
             e.printStackTrace();
